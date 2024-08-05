663841355ab96ba8d5142a6ce63e6b0f79a58f5b
Viktoriia Sergeeva <mothspaws@Viktoriias-MacBook-Pro.local>
2024-08-05 22:29:38 +0200

    task_1 use base HTML tags

--- react-introduction/client/src/App.js
+++ react-introduction/client/src/App.js
@@ -1,23 +1,287 @@
-import logo from './logo.svg';
 import './App.css';
+
+const recipes = {
+  name: "Babiččiny recepty",
+  recipes: [
+    {
+      "name": "Salát z naklíčené čočky",
+      "description": "Mrkev, okurku a papriku nakrájejte na malé kostičky a dejte do větší mísy spolu s naklíčenou čočkou. Cibuli nakrájejte najemno a přidejte k zelenině. Přisypte nasekanou petrželku. V misce nebo hrníčku důkladně promíchejte lák z okurek, olivový olej a med. Zálivku nalijte do mísy a důkladně promíchejte. Na závěr dochuťte solí a pepřem.",
+      "imgUri": "https://zachranjidlo.cz/wp-content/uploads/dsc02309-1-e1652694711486-1024x433-1200x500-c-default.jpg",
+      "ingredients": [
+        {
+          "id": "c6ea4fb4d330d32b",
+          "amount": 200,
+          "unit": "g"
+        },
+        {
+          "id": "01ed1245cdfd005a",
+          "amount": 1,
+          "unit": "ks"
+        },
+        {
+          "id": "95ff7d609062d4db",
+          "amount": 0.5,
+          "unit": "ks"
+        },
+        {
+          "id": "73fd3db102b7803d",
+          "amount": 0.5,
+          "unit": "ks"
+        },
+        {
+          "id": "9416b19e1bd2cd11",
+          "amount": 1,
+          "unit": "ks"
+        },
+        {
+          "id": "426371a4bbc275f1",
+          "amount": 1,
+          "unit": "hrst"
+        },
+        {
+          "id": "e0e1a7f5ccef3dff",
+          "amount": 3,
+          "unit": "lžíce"
+        },
+        {
+          "id": "d8e6bbd4d6038494",
+          "amount": 2,
+          "unit": "lžíce"
+        },
+        {
+          "id": "defe5185b23ca985",
+          "amount": 1,
+          "unit": "lžíce"
+        },
+        {
+          "id": "3f702872fb8e99f8",
+          "amount": 1,
+          "unit": "špetka"
+        },
+        {
+          "id": "cd99517791018390",
+          "amount": 1,
+          "unit": "špetka"
+        }
+      ],
+      "id": "b6c21cf8807dd356"
+    },
+    {
+      "name": "Ovesné placičky",
+      "description": "Cibuli oloupejte a nastrouhejte nahrubo. Mrkev důkladně umyjte a nastrouhejte najemno spolu s česnekem. V míse smíchejte vločky, cibuli, mrkev, česnek a koření. Přidejte strouhanku a důkladně promíchejte, ideálně rukou tak, aby vznikla jednotná směs. Pokud je směs příliš suchá, přidejte trošku vody, pokud je příliš mokrá, přidejte trošku strouhanky. Na pánvi rozpalte olej, ze směsi vytvarujte malé placičky a smažte z obou stran dozlatova.",
+      "imgUri": "https://zachranjidlo.cz/wp-content/uploads/dsc-0516-1-1024x480-1200x500-c-default.jpg",
+      "ingredients": [
+        {
+          "id": "f9996cfda5568262",
+          "amount": 2,
+          "unit": "hrnky"
+        },
+        {
+          "id": "518aea069b179f29",
+          "amount": 2,
+          "unit": "ks"
+        },
+        {
+          "id": "01ed1245cdfd005a",
+          "amount": 2,
+          "unit": "ks"
+        },
+        {
+          "id": "157003980c0a437a",
+          "amount": 4,
+          "unit": "lžíce"
+        },
+        {
+          "id": "2220a6eb35e31dc6",
+          "amount": 2,
+          "unit": "ks"
+        },
+        {
+          "id": "0b9ccac3b2f733cc",
+          "amount": 1,
+          "unit": "lžička"
+        },
+        {
+          "id": "dfa4b721efe898a5",
+          "amount": 1,
+          "unit": "lžička"
+        },
+        {
+          "id": "97f5d8e88343e612",
+          "amount": 1,
+          "unit": "lžička"
+        },
+        {
+          "id": "3f702872fb8e99f8",
+          "amount": 1,
+          "unit": "lžička"
+        },
+        {
+          "id": "cd99517791018390",
+          "amount": 0.25,
+          "unit": "lžičky"
+        },
+        {
+          "id": "40d227ce8a379758",
+          "amount": 100,
+          "unit": "ml"
+        }
+      ],
+      "id": "854f2f3cb8954916"
+    },
+    {
+      "name": "Barbecue burger ze zbylého kuřete",
+      "description": "Rozehřejte troubu na 240 °C. Obrané drůbeží maso natrhejte na vlákna, zamíchejte s barbecue omáčkou a rozprostřete do pekáčku. Dejte do trouby a pečte asi 10 minut. Rozpůlené bulky opečte na rozpálené pánvi na sucho z obou stran. Limetu umyjte, nastrouhejte kůru, šťávu vymačkejte a obojí smíchejte s majonézou. Pomocí škrabky udělejte z mrkve tenké proužky. Přendejte je do misky, přidejte špetku soli a pepře a pár kapek limety a promíchejte. Začněte skládat burger. Obě půlky bulek pomažte limetovou majonézou. Na spodní polovinu bulky navrstvěte natrhaný koriandr, na plátky nasekanou chilli papričku, mrkvové proužky, tenká kolečka šalotky a plátek rajčete. Nakonec přidejte vrstvu zapečeného bbq kuřete a plátek cheddaru. Přiklopte vrchní polovinou bulky a podávejte.",
+      "imgUri": "https://zachranjidlo.cz/wp-content/uploads/bbq-kure-burger-1024x493-1200x500-c-default.png",
+      "ingredients": [
+        {
+          "id": "9bb6644a5520cf8e",
+          "amount": 400,
+          "unit": "g"
+        },
+        {
+          "id": "a6e8d310c6628d31",
+          "amount": 4,
+          "unit": "ks"
+        },
+        {
+          "id": "f6d6a4104a5194d9",
+          "amount": 100,
+          "unit": "g"
+        },
+        {
+          "id": "87dbea8746ecb15d",
+          "amount": 1,
+          "unit": "ks"
+        },
+        {
+          "id": "59a3f03b35e69690",
+          "amount": 3,
+          "unit": "snítka"
+        },
+        {
+          "id": "f34bba8dd7088dff",
+          "amount": 80,
+          "unit": "ml"
+        },
+        {
+          "id": "01ed1245cdfd005a",
+          "amount": 1,
+          "unit": "ks"
+        },
+        {
+          "id": "b08135ccef590fd6",
+          "amount": 1,
+          "unit": "ks"
+        },
+        {
+          "id": "9c0957e5273de89a",
+          "amount": 1,
+          "unit": "ks"
+        },
+        {
+          "id": "cfb695a5d686cb2e",
+          "amount": 2,
+          "unit": "ks"
+        },
+        {
+          "id": "5cfea498406de1b7",
+          "amount": 2,
+          "unit": "lžíce"
+        }
+      ],
+      "id": "1ae20af4cfa8efc4"
+    },
+    {
+      "name": "Chřestová polévka",
+      "description": "Ve velkém hrnci přiveďte k varu 1000 ml vody. Omytý chřest oloupejte, odlomte dřevnaté konce a dejte je pomalu vařit. Z chřestu odkrojte hlavičky a zbytek chřestu pokrájejte na 1 cm kousky. V hrnci rozehřejte 20 g másla a hlavičky na něm lehce opečte, dejte stranou. Na zbylém másle pomalu restujte nadrobno pokrájenou šalotku a prolisovaný stroužek česneku dosklovata. Přidejte pokrájené stonky chřestu a pomalu restujte dalších 5 minut. Přilijte vývar z chřestu a vařte 15 minut. Vše důkladně promixujte a vraťte do hrnce a vypněte plotýnku. V misce smíchejte žloutky s majonézou a vínem. Směs pomalu vlijte do hrnce s polévkou a za stálého míchání dokud polévka lehce nezhoustne. Lehce polévku přihřejte a dochuťte solí a citronovou šťávou. Polévku ozdobte opečenými hlavičkami chřestu a výhonky hrášku. Servírujte s opečenou bagetou.",
+      "imgUri": "https://zachranjidlo.cz/wp-content/uploads/chrestpolevka-1024x427-1200x500-c-default.png",
+      "ingredients": [
+        {
+          "id": "8df05c04dec4ad46",
+          "amount": 500,
+          "unit": "g"
+        },
+        {
+          "id": "6740ae2c9acfa794",
+          "amount": 70,
+          "unit": "g"
+        },
+        {
+          "id": "87dbea8746ecb15d",
+          "amount": 2,
+          "unit": "ks"
+        },
+        {
+          "id": "2220a6eb35e31dc6",
+          "amount": 1,
+          "unit": "ks"
+        },
+        {
+          "id": "88c04efef87fecae",
+          "amount": 50,
+          "unit": "ml"
+        },
+        {
+          "id": "7aea5d7072d9f488",
+          "amount": 2,
+          "unit": "ks"
+        },
+        {
+          "id": "21a3625a381c4cf5",
+          "amount": 4,
+          "unit": "lžíce"
+        },
+        {
+          "id": "86b1f4a7c798eb04",
+          "amount": 1,
+          "unit": "lžíce"
+        },
+        {
+          "id": "7c45f9808efc936a",
+          "amount": 1,
+          "unit": "lžíce"
+        },
+        {
+          "id": "3f702872fb8e99f8",
+          "amount": 1,
+          "unit": "špetka"
+        },
+        {
+          "id": "cd99517791018390",
+          "amount": 1,
+          "unit": "špetka"
+        },
+        {
+          "id": "53dae6e5a0076b15",
+          "amount": 1,
+          "unit": "ks"
+        }
+      ],
+      "id": "2dd281dff25fe765"
+    }
+  ]
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{recipes.name}</h1>
+      <h2>Vyběr si něco dobrého</h2>
+      <div className="recipe-list">
+        {recipes.recipes.map((recipe) => (
+          <div key={recipe.id} className="recipe">
+            <h3>{recipe.name}</h3>
+            <img src={recipe.imgUri} alt={recipe.name} />
+            <p>{recipe.description}</p>
+            <ul className="ingredients">
+              {recipe.ingredients.map((ingredient) => (
+                <li key={ingredient.id}>{ingredient.amount} {ingredient.unit}</li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
