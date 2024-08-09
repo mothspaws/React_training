cc05ecdc369bcf700cfbe15bbc99fe897dd42c7f
Viktoriia Sergeeva <mothspaws@Viktoriias-MacBook-Pro.local>
2024-08-09 09:29:51 +0200

    fix display foto

--- react-introduction/client/src/components/Recipe.js
+++ react-introduction/client/src/components/Recipe.js
@@ -1,9 +1,10 @@
 import { Card } from "react-bootstrap";
+import restaurantImage from './storage/restaurant.png';
 
 function Recipe({ recipe }) {
     return (
         <Card className="card">
-            <Card.Img className="card-image" variant="top" src={'./storage/restaurant.png'} />
+            <Card.Img className="card-image" variant="top" src={restaurantImage} />
             <Card.Body className="card-body">
                 <Card.Title className="card-title">{recipe.name}</Card.Title>
                 <Card.Text className="card-text">
